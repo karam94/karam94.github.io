583567049ed0dd8294542b222a51f105549ae81d
Karam Kabbara <contact@karam.io>
2020-10-31 23:04:17 +0000

    Modified post card and reduced width

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -13,23 +13,17 @@
     </div>
     <div class="post-card__content">
       <h2 class="post-card__title" v-html="post.title" />
-      <p class="post-card__description" v-html="post.description" />
-
-      <PostMeta class="post-card__meta" :post="post" />
       <PostTags class="post-card__tags" :post="post" />
-
       <g-link class="post-card__link" :to="post.path">Link</g-link>
     </div>
   </div>
 </template>
 
 <script>
-import PostMeta from "~/components/PostMeta";
 import PostTags from "~/components/PostTags";
 
 export default {
   components: {
-    PostMeta,
     PostTags,
   },
   props: ["post"],
